0ab0ba31ec1f45e067184b794974517a466be125
Freanu Peria <magnussonfrancis@gmail.com>
2022-04-28 19:56:10 +0800

    shows masks and shields predictions. WIP.

--- core/predictor.py
+++ core/predictor.py
@@ -15,26 +15,11 @@ def convertBack(x, y, w, h):
 def cvDrawBoxes(detections, img):
     # Colored labels dictionary
     color_dict = {
-        'person' : [0, 255, 255], 'bicycle': [238, 123, 158], 'car' : [24, 245, 217], 'motorbike' : [224, 119, 227],
-        'aeroplane' : [154, 52, 104], 'bus' : [179, 50, 247], 'train' : [180, 164, 5], 'truck' : [82, 42, 106],
-        'boat' : [201, 25, 52], 'traffic light' : [62, 17, 209], 'fire hydrant' : [60, 68, 169], 'stop sign' : [199, 113, 167],
-        'parking meter' : [19, 71, 68], 'bench' : [161, 83, 182], 'bird' : [75, 6, 145], 'cat' : [100, 64, 151],
-        'dog' : [156, 116, 171], 'horse' : [88, 9, 123], 'sheep' : [181, 86, 222], 'cow' : [116, 238, 87],'elephant' : [74, 90, 143],
-        'bear' : [249, 157, 47], 'zebra' : [26, 101, 131], 'giraffe' : [195, 130, 181], 'backpack' : [242, 52, 233],
-        'umbrella' : [131, 11, 189], 'handbag' : [221, 229, 176], 'tie' : [193, 56, 44], 'suitcase' : [139, 53, 137],
-        'frisbee' : [102, 208, 40], 'skis' : [61, 50, 7], 'snowboard' : [65, 82, 186], 'sports ball' : [65, 82, 186],
-        'kite' : [153, 254, 81],'baseball bat' : [233, 80, 195],'baseball glove' : [165, 179, 213],'skateboard' : [57, 65, 211],
-        'surfboard' : [98, 255, 164],'tennis racket' : [205, 219, 146],'bottle' : [140, 138, 172],'wine glass' : [23, 53, 119],
-        'cup' : [102, 215, 88],'fork' : [198, 204, 245],'knife' : [183, 132, 233],'spoon' : [14, 87, 125],
-        'bowl' : [221, 43, 104],'banana' : [181, 215, 6],'apple' : [16, 139, 183],'sandwich' : [150, 136, 166],'orange' : [219, 144, 1],
-        'broccoli' : [123, 226, 195],'carrot' : [230, 45, 209],'hot dog' : [252, 215, 56],'pizza' : [234, 170, 131],
-        'donut' : [36, 208, 234],'cake' : [19, 24, 2],'chair' : [115, 184, 234],'sofa' : [125, 238, 12],
-        'pottedplant' : [57, 226, 76],'bed' : [77, 31, 134],'diningtable' : [208, 202, 204],'toilet' : [208, 202, 204],
-        'tvmonitor' : [208, 202, 204],'laptop' : [159, 149, 163],'mouse' : [148, 148, 87],'remote' : [171, 107, 183],
-        'keyboard' : [33, 154, 135],'cell phone' : [206, 209, 108],'microwave' : [206, 209, 108],'oven' : [97, 246, 15],
-        'toaster' : [147, 140, 184],'sink' : [157, 58, 24],'refrigerator' : [117, 145, 137],'book' : [155, 129, 244],
-        'clock' : [53, 61, 6],'vase' : [145, 75, 152],'scissors' : [8, 140, 38],'teddy bear' : [37, 61, 220],
-        'hair drier' : [129, 12, 229],'toothbrush' : [11, 126, 158], "with_mask": [0, 255, 255], "without_mask": [156, 116, 171]
+        'face_shield' : [206, 209, 108],
+        'no face_shield' : [245, 239, 243],
+        "face_mask": [0, 255, 255], 
+        "no face_mask": [0, 255, 255], 
+        'person' : [50, 205, 50]
     }
     
 
@@ -63,15 +48,19 @@ def cvDrawBoxes(detections, img):
 class Darknet:
 
     def __init__(self, frame_width, frame_height):
-        # load pretrained model 
-        # configPath = "./cfg/yolov4.cfg"
-        # weightPath = "./yolov4.weights"
-        # metaPath = "./cfg/coco.data"
+        self.frame_width = frame_width
+        self.frame_height = frame_height
+        self.darknet_image = darknet.make_image(self.frame_width, self.frame_height, 3)
+        
+        # files for masks and shields 
+        # change directories below accordingly
+        configPath = "./cw/yolov4-tiny-3l-obj.cfg"
+        weightPath = "./cw/yolov4-tiny-3l-obj_bestAPR27.weights"
+        metaPath = "./cw/obj.data"
 
-        # custom trained models
-        configPath = "./cfg/yolov4-tiny-custom-mask.cfg"                                 # Path to cfg
-        weightPath = "./mask.weights"                                 # Path to weights
-        metaPath = "./cfg/mask.data" 
+        # files for pretrained model
+        
+
 
         if not os.path.exists(configPath):                              # Checks whether file exists otherwise return ValueError
             raise ValueError("Invalid config path `" +
@@ -86,10 +75,6 @@ class Darknet:
         network, class_names, class_colors = darknet.load_network(configPath,  metaPath, weightPath, batch_size=1)
         self.network = network
         self.class_names = class_names
-        self.class_colors = class_colors
-        self.frame_width = frame_width
-        self.frame_height = frame_height
-        self.darknet_image = darknet.make_image(self.frame_width, self.frame_height, 3)
 
     def predict(self, image):
         frame_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
